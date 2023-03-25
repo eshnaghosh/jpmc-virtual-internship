41560b45baa457369ba640cde1c52804eb797a2d
Eshna Ghosh <64518970+eshnaghosh@users.noreply.github.com>
2023-03-25 19:26:47 +0530

    Update client.py

--- client.py
+++ client.py
@@ -18,42 +18,52 @@
 #  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 #  DEALINGS IN THE SOFTWARE.
 
+import urllib.request
+import time
 import json
 import random
-import urllib.request
 
 # Server API URLs
-QUERY = "http://localhost:8080/query?id={}"
+QUERY = "http://localhost:8085/query?id={}"
 
 # 500 server request
 N = 500
 
-
 def getDataPoint(quote):
-    """ Produce all the needed values to generate a datapoint """
-    """ ------------- Update this function ------------- """
-    stock = quote['stock']
-    bid_price = float(quote['top_bid']['price'])
-    ask_price = float(quote['top_ask']['price'])
-    price = bid_price
-    return stock, bid_price, ask_price, price
-
+	""" Produce all of the needed values to generate a datapoint """
+	""" ------------- Update this function ------------- """
+	stock = quote['stock']
+	bid_price = float(quote['top_bid']['price'])
+	ask_price = float(quote['top_ask']['price'])
+	price = (bid_price + ask_price) / 2
+	return stock, bid_price, ask_price, price
 
 def getRatio(price_a, price_b):
-    """ Get ratio of price_a and price_b """
-    """ ------------- Update this function ------------- """
-    return 1
-
+	""" Get ratio of price_a and price_b """
+	""" ------------- Update this function ------------- """
+	""" Also create some unit tests for this function in client_test.py """
+	# ZeroDivisionError exception handler
+	try:
+		ratio = price_a / price_b
+		return ratio
+	except Exception as ZeroDivisionError:
+		print(f"returning as {ZeroDivisionError} , debug info : price_b is zero")
+		return
 
 # Main
 if __name__ == "__main__":
-    # Query the price once every N seconds.
-    for _ in iter(range(N)):
-        quotes = json.loads(urllib.request.urlopen(QUERY.format(random.random())).read())
 
-        """ ----------- Update to get the ratio --------------- """
-        for quote in quotes:
-            stock, bid_price, ask_price, price = getDataPoint(quote)
-            print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
+	# Query the price once every N seconds.
+	for _ in iter(range(N)):
+		quotes = json.loads(urllib.request.urlopen(QUERY.format(random.random())).read())
 
-        print("Ratio %s" % getRatio(price, price))
+		""" ----------- Update to get the ratio --------------- """
+		# prices dictionery holds the key as stock name and value as price of stock
+		prices = {}
+
+		for quote in quotes:
+			stock, bid_price, ask_price, price = getDataPoint(quote)
+			prices[stock] = price
+			print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
+
+		print("Ratio %s" % getRatio(prices['ABC'], prices['DEF']))
